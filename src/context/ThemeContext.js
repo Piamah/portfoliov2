06f4538db72e547931eedf2198505edf62bce068
Piamah <athelec@gmail.com>
2025-01-24 16:13:02 +0100

    Modifications themecontext publication

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,15 +3,22 @@ import React, { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
+    const isBrowser = typeof window !== "undefined";
+
     const [isDark, setIsDark] = useState(() => {
-        const savedTheme = window.localStorage.getItem("theme");
-        return savedTheme === "dark";
+        if (isBrowser) {
+            const savedTheme = window.localStorage.getItem("theme");
+            return savedTheme === "dark";
+        }
+        return false;
     });
 
     useEffect(() => {
-        document.body.classList.toggle("dark", isDark);
-        window.localStorage.setItem("theme", isDark ? "dark" : "light");
-    }, [isDark]);
+        if (isBrowser) {
+            document.body.classList.toggle("dark", isDark);
+            window.localStorage.setItem("theme", isDark ? "dark" : "light");
+        }
+    }, [isDark, isBrowser]);
 
     return (
         <ThemeContext.Provider value={{ isDark, toggleTheme: () => setIsDark(!isDark) }}>
