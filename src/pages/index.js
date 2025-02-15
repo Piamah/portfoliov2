1c131dbe29d2e581445e8d2bce68139e2b2ec948
Piamah <athelec@gmail.com>
2025-02-15 17:42:07 +0100

    emplacement verif

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,7 @@ export default function Home() {
           <title>Athénaïs Lecomte - Portfolio</title>
           <meta name="description" content="Portfolio d'Athénaïs Lecomte', développeur web : mes projets et mes compétences." />
           <meta name='keywords' content='portfolio, athenais lecomte, développeur, projets, compétences' />
+          <meta name="google-site-verification" content="II_LbYqDDZTEXdh4FzvYNuGS92gzsauEOEhB98kUb8E" />
         </Helmet>
 
         <section id="header-section">
