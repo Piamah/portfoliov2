0468ebb2de527e7d1c824e0f63a13d2296f2ef93
Piamah <athelec@gmail.com>
2025-02-18 18:04:46 +0100

    Ajout et modification balises meta/twitter et retest changement titre

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,37 @@ export default function Home() {
         <Helmet>
           <html lang="fr" />
           <title>Athénaïs Lecomte  - Développeur Web - Portfolio</title>
-          <meta property="og:title" content="Athénaïs Lecomte - Développeur Web - Portfolio" />
+
           <meta name="description" content="Portfolio d'Athénaïs Lecomte', développeur web : mes projets et mes compétences." />
           <meta name='keywords' content='portfolio, athenais lecomte, développeur, projets, compétences' />
           <meta name="google-site-verification" content="II_LbYqDDZTEXdh4FzvYNuGS92gzsauEOEhB98kUb8E" />
-        </Helmet>
 
+          <meta property="og:title" content="Athénaïs Lecomte  - Développeur Web - Portfolio" />
+          <meta property="og:description" content="Portfolio d'Athénaïs Lecomte', développeur web : mes projets et mes compétences." />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://athenais-lecomte.netlify.app/" />
+          <meta property="og:locale" content="fr_FR" />
+
+          <meta name="twitter:title" content="Athénaïs Lecomte  - Développeur Web - Portfolio" />
+          <meta name="twitter:description" content="Portfolio d'Athénaïs Lecomte', développeur web : mes projets et mes compétences." />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:url" content="https://athenais-lecomte.netlify.app/" />
+
+          <script type="application/ld+json">
+            {JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "ProfessionalService",
+              "name": "Développeur web",
+              "description": "Développeur web",
+              "url": "https://athenais-lecomte.netlify.app/",
+              "employee": {
+                "@type": "Person",
+                "name": "Athénaïs Lecomte",
+                "jobTitle": "Développeur Web"
+              }
+            })}
+          </script>
+        </Helmet>
         <section id="header-section">
           <Header />
         </section>
