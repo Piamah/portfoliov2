0d22b03acabbff322b3934cd0c3e0d594a532989
Piamah <athelec@gmail.com>
2025-02-19 14:52:46 +0100

    Modification helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/index.scss';
+import { Helmet } from 'react-helmet';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Banner from '../components/Banner';
@@ -9,47 +9,31 @@ import Cards from '../components/Cards';
 import Contact from '../components/Contact';
 import Socials from '../components/Socials';
 import { ThemeProvider } from "../context/ThemeContext.js";
-import { Helmet } from "react-helmet";
-import '../multiLang'
+import '../styles/index.scss';
+import '../multiLang';
 
 export default function Home() {
   return (
     <div id="main">
+      <Helmet>
+        <html lang="fr" />
+        <title>Athénaïs Lecomte - Développeur Web - Portfolio</title>
+
+        <meta name="description" content="Portfolio d'Athénaïs Lecomte, développeur web : mes projets et mes compétences." />
+        <meta name="keywords" content="portfolio, athenais lecomte, développeur, projets, compétences" />
+        <meta name="google-site-verification" content="II_LbYqDDZTEXdh4FzvYNuGS92gzsauEOEhB98kUb8E" />
+
+        <meta property="og:title" content="Athénaïs Lecomte - Développeur Web - Portfolio" />
+        <meta property="og:description" content="Portfolio d'Athénaïs Lecomte, développeur web : mes projets et mes compétences." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://athenais-lecomte.netlify.app/" />
+        <meta property="og:locale" content="fr_FR" />
+
+        <meta name="twitter:title" content="Athénaïs Lecomte - Développeur Web - Portfolio" />
+        <meta name="twitter:description" content="Portfolio d'Athénaïs Lecomte, développeur web : mes projets et mes compétences." />
+      </Helmet>
+
       <ThemeProvider>
-        <Helmet>
-          <html lang="fr" />
-          <title>Athénaïs Lecomte  - Développeur Web - Portfolio</title>
-
-          <meta name="description" content="Portfolio d'Athénaïs Lecomte', développeur web : mes projets et mes compétences." />
-          <meta name='keywords' content='portfolio, athenais lecomte, développeur, projets, compétences' />
-          <meta name="google-site-verification" content="II_LbYqDDZTEXdh4FzvYNuGS92gzsauEOEhB98kUb8E" />
-
-          <meta property="og:title" content="Athénaïs Lecomte  - Développeur Web - Portfolio" />
-          <meta property="og:description" content="Portfolio d'Athénaïs Lecomte', développeur web : mes projets et mes compétences." />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://athenais-lecomte.netlify.app/" />
-          <meta property="og:locale" content="fr_FR" />
-
-          <meta name="twitter:title" content="Athénaïs Lecomte  - Développeur Web - Portfolio" />
-          <meta name="twitter:description" content="Portfolio d'Athénaïs Lecomte', développeur web : mes projets et mes compétences." />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:url" content="https://athenais-lecomte.netlify.app/" />
-
-          <script type="application/ld+json">
-            {JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "ProfessionalService",
-              "name": "Développeur web",
-              "description": "Développeur web",
-              "url": "https://athenais-lecomte.netlify.app/",
-              "employee": {
-                "@type": "Person",
-                "name": "Athénaïs Lecomte",
-                "jobTitle": "Développeur Web"
-              }
-            })}
-          </script>
-        </Helmet>
         <section id="header-section">
           <Header />
         </section>
